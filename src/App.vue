2330bcea482695c25497a6e737efe22278b6f2b3
Sosias <trambusti.stefano@gmail.com>
2020-08-21 23:49:10 +0200

    create base functions

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,105 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+      <div class="custom-file">
+        <input type="file" class="custom-file-input" id="customFile" @change="onFileChange" />
+        <label class="custom-file-label" for="customFile">{{fileSelectText}}</label>
+      </div>
+
+      <div id="preview">
+        <img v-if="url" :src="url" />
+      </div>
+
+      <tmxViewer v-if="preparedToCSVData" :data=preparedToCSVData />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TmxViewer from './components/TmxViewer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TmxViewer
+  },
+  data() {
+    return {
+      readXml: null,
+      url: null,
+      data: null,
+      preparedToCSVData: null,
+      fileSelectText: "Choose a file"
+    }
+  },
+  methods: {
+    onFileChange(e) {
+      const file = e.target.files[0];
+      this.fileSelectText = e.target.files[0].name
+      var reader = new FileReader();
+      reader.onload = function(e) {
+          let domElement = this.getXMLdomElement(e.target.result)
+          this.data = this.parseData(domElement)
+          this.preparedToCSVData = this.prepareCSVArray(this.data)
+          console.log(this.preparedToCSVData)
+      }.bind(this)
+      reader.readAsText(file);
+    },
+    getXMLdomElement(xmlFile){
+      var parser = new DOMParser();
+      var doc = parser.parseFromString(xmlFile, "application/xml");
+      return doc;
+    },
+    parseData(domElement){
+      let languages = {}
+      var body = domElement.getElementsByTagName("body")[0]
+      var tus = body.getElementsByTagName("tu")
+
+      var translations = Array.from(tus).map((el)=>{
+        return Array.from(el.getElementsByTagName("tuv")).map((tuvEl)=>{
+
+          let language = tuvEl.getAttribute("xml:lang").toUpperCase()
+          let segElements = tuvEl.getElementsByTagName("seg")
+          let content = segElements.length ? segElements[0].innerHTML : ""
+          
+          //save the current language label and increase the language "encounter" counter
+          languages[language]===undefined? languages[language]=0 : languages[language]++
+
+          return {lang: language, content: content}
+        })
+      })
+
+      let data = {
+        languages: languages,
+        translations: translations
+      }
+
+      return data
+    },
+    prepareCSVArray(data){
+      let langs = Object.keys(data.languages)
+
+      let body = data.translations.map((dataRow)=>{
+        let newRow = new Array(langs.length)
+
+        dataRow.map((dataItem)=>{
+          // insert the translation content in the rigth position of the row (respective to the language)
+          // if doesn't exist the language, don't save it
+          let entryIndex = langs.indexOf(dataItem.lang)
+          entryIndex > -1 ? newRow[langs.indexOf(dataItem.lang)] = dataItem.content : console.error("some content doesn't have a respective language "+dataItem.content)
+        })
+
+        return newRow
+      })
+
+      return {header: langs, body: body}
+    }
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 1.5em;
 }
 </style>
---
+++ src/components/TmxViewer.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <table class="viewer table table-striped">
+    <thead class="thead-light">
+      <tr>
+        <th scope="col">id</th>
+        <th :key=key v-for="(item, key) in data.header" scope="col">{{item}}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <template v-for="(row, key) in data.body">
+          <tr :key=key sope="row">
+            <td>{{key}}</td>
+            <td v-for="(item, key) in row" :key=key>{{item}}</td>
+          </tr>
+      </template>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'TmxViewer',
+  props: {
+    data: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
