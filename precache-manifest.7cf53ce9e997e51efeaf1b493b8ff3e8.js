72a9f4c78189f70a34efd83b03881822d85460b1
sosias <sosias@protonmail.com>
2020-08-22 13:34:16 +0200

    Deploy

---
+++ precache-manifest.7cf53ce9e997e51efeaf1b493b8ff3e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2fd5d0c750ac15bb82",
+    "url": "/tmx-tool/css/app.48d98bee.css"
+  },
+  {
+    "revision": "422f4e05322d87046dbb",
+    "url": "/tmx-tool/css/chunk-vendors.6612108b.css"
+  },
+  {
+    "revision": "91a16149c2111e0a36ce21fff36faefb",
+    "url": "/tmx-tool/index.html"
+  },
+  {
+    "revision": "8b2fd5d0c750ac15bb82",
+    "url": "/tmx-tool/js/app.20469918.js"
+  },
+  {
+    "revision": "422f4e05322d87046dbb",
+    "url": "/tmx-tool/js/chunk-vendors.1497861f.js"
+  },
+  {
+    "revision": "2ef492356d3dc1625b469641497fe6d4",
+    "url": "/tmx-tool/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tmx-tool/robots.txt"
+  },
+  {
+    "revision": "9e39b6f19b61fa71340c548eb69d3835",
+    "url": "/tmx-tool/site.webmanifest"
+  }
+]);
